7a524210235578d43ca115b2fd52d348d8c188aa
Andres <andres.gonzalez2392@gmail.com>
2022-07-27 15:29:37 -0400

    BLUE THEME - Font and text alignment change
    BLUE THEME - Font and text alignment change

--- assets/js/main.js
+++ assets/js/main.js
@@ -200,3 +200,19 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+/*==================== SEND CONTACT EMAIL ====================*/ 
+
+function sendEmail(){
+    Email.send({
+        Host : "smtp.yourisp.com",
+        Username : "username",
+        Password : "password",
+        To : 'them@website.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
